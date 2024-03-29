8308d5a76105d7a0890f5ca91b608c22ab782595
Nick Spain <nspain@fastmail.com>
2024-03-29 11:58:49 +1100

    First pass at implementing method/function overloading in Ruby

--- lib/straw.rb
+++ lib/straw.rb
@@ -3,6 +3,69 @@
 require_relative "straw/version"
 
 module Straw
-  class Error < StandardError; end
-  # Your code goes here...
+    class Error < StandardError
+    end
+
+    class NoMatchingOverloadError < Error
+        attr_reader :name, :arity
+
+        def initialize(name:, arity:)
+            @method_name = name
+            @arity = arity
+            super("No matching overload for #{name} with arity #{arity}")
+        end
+    end
+
+    class UnhandledDefinitionOriginError < Error
+        attr_reader :origin
+        def initialize(origin)
+            @origin = origin
+            super(
+                "Origin #{origin} is not handled for method or function overloading"
+            )
+        end
+    end
+
+    def overload(method_name)
+        original = case self
+        when Class
+            instance_method(method_name)
+        when Module
+            method(method_name)
+        else
+            raise UnhandledDefinitionOriginError.new(self.class.name)
+        end
+
+        @overload_definitions ||= Hash.new { |h, k| h[k] = [] }
+        @overload_definitions[original.name] << original
+        overload_definitions = @overload_definitions
+        body = Proc.new do |*args, &blk|
+            required_arity = args.length
+            selected_method = overload_definitions[method_name].find do |defn|
+                defn.arity == required_arity
+            end
+
+            if selected_method.nil?
+                raise(
+                    NoMatchingOverloadError.new(
+                        name: method_name.name,
+                        arity: required_arity
+                    )
+                )
+            end
+
+            if !self.is_a?(Module)
+                selected_method = selected_method.bind(self)
+            end
+
+            selected_method.call(*args, &blk)
+        end
+
+        case self
+        when Class
+            define_method(method_name, body)
+        when Module
+            define_singleton_method(method_name, body)
+        end
+    end
 end
--- test/test_straw.rb
+++ test/test_straw.rb
@@ -1,13 +1,57 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "./test_helper"
 
 class TestStraw < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Straw::VERSION
-  end
+    def test_it_handles_method_overloads
+        dummy = Class
+          .new do
+              extend(Straw)
 
-  def test_it_does_something_useful
-    assert false
-  end
+              overload def test_method(first_arg)
+                  1
+              end
+
+              overload def test_method(first_arg, second_arg)
+                  2
+              end
+
+              overload def test_method(first_arg, second_arg, third_arg)
+                  3
+              end
+          end
+          .new
+
+        assert_equal(dummy.test_method("test"), 1)
+        assert_equal(dummy.test_method("test", "test"), 2)
+        assert_equal(dummy.test_method("test", "test", "test"), 3)
+        assert_raises(Straw::NoMatchingOverloadError) {
+            dummy.test_method("test", "test", "test", "test")
+        }
+    end
+
+    def test_it_handles_function_overloads
+        dummy = Module.new do
+            extend(Straw)
+
+            overload def self.test(first_arg)
+                1
+            end
+
+            overload def self.test(first_arg, second_arg)
+                2
+            end
+
+            overload def self.test(first_arg, second_arg, third_arg)
+                3
+            end
+        end
+
+        assert_equal(dummy.test("test"), 1)
+        assert_equal(dummy.test("test", "test"), 2)
+        assert_equal(dummy.test("test", "test", "test"), 3)
+        assert_raises(Straw::NoMatchingOverloadError) {
+            dummy.test("test", "test", "test", "test")
+        }
+    end
 end
